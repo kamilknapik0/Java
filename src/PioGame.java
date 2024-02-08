6b62cf615f87344590823b524f6831061ae64420
kamil <147108855+kamilknapik0@users.noreply.github.com>
2024-02-08 14:56:30 +0100

    Update PioGame.java

--- src/PioGame.java
+++ src/PioGame.java
@@ -1,6 +1,5 @@
 public class PioGame {
     public static void main(String[] args) {
-        //gra w odgadywanie liczby z kostki
         Game game = new Game();
 
         game.addPlayer(new PlayerComp("Janusz"));
